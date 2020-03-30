9324a3084d50b65cf3f358bc51b2cd2ddb8a518d
Alice (KitayskiyAS) <alice@example.com>
2020-03-30 13:08:42 +0300

    code: double

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a, b;
+    double a, b;
     int max;
+    int min;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
     cout << "max = " << max << '\n';
+    cout << "min = " << min << '\n';
     return 0;
 }
