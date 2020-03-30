e498812ac66b2bb3bbb3764bc4a3814d0f412c72
Bob (KitayskiyAS) <bob@example.com>
2020-03-30 12:47:54 +0300

    code: min

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     int max;
+    int min;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
     cout << "max = " << max << '\n';
+    cout << "min = " << min << '\n';
     return 0;
 }
