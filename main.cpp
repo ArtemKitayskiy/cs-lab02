9e84bd21fcf5f5a0458e270c8ac7b6e449602ae8
Alice (KitayskiyAS) <alice@example.com>
2020-03-30 12:45:54 +0300

    code: max

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ int main()
 {
     cout << "Enter A and B: ";
     int a, b;
+    int max;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
+    cout << "max = " << max << '\n';
     return 0;
 }
